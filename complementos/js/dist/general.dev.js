552dfac158972733416cd9ff58c4f4407613caf7
unknown <yoelreyes54@gmail.com>
2020-10-17 07:03:27 -0500

    subida del proyecto spaceall

---
+++ complementos/js/dist/general.dev.js
@@ -0,0 +1,59 @@
+"use strict";
+
+// Side Menu
+var sideNav = document.querySelector('.sidenav');
+M.Sidenav.init(sideNav, {}); // Slider
+
+var slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+  indicators: false,
+  height: 500,
+  transition: 500,
+  interval: 6000
+}); // Scrollspy
+
+var ss = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(ss, {}); // Material Boxed
+
+var mb = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(mb, {}); //dropdown-navbar
+
+var dropdowns = document.querySelectorAll('.dropdown-trigger');
+
+for (var i = 0; i < dropdowns.length; i++) {
+  M.Dropdown.init(dropdowns[i]);
+}
+/*intancia de navbar
+  var navbar = new Vue({
+   el: '#xnavbarvue',
+   data: { 
+      iniciarSesion : false,
+      cerrarSesion : false,
+      configuracion : false
+   },
+    
+   mounted(){
+   
+       if (localStorage.length > 0) {
+         console.log("localStorage lleno");
+         this.configuracion = !this.configuracion
+         this.cerrarSesion = !this.cerrarSesion
+   
+        }else{
+         this.iniciarSesion = !this.iniciarSesion
+        }
+     
+   },
+   methods: { 
+     cerrarSession(){
+       localStorage.clear();
+       this.configuracion = !this.configuracion
+       this.cerrarSesion = !this.cerrarSesion
+   
+     
+     }
+   
+   },
+   
+   }); //intancia de navbar
+   */
---
+++ funciones/conexiondb.php
@@ -0,0 +1,5 @@
+<?php
+
+$con = new mysqli("localhost", "root", "", "space");
+//$mysqli->select_db("dog");
+?>
